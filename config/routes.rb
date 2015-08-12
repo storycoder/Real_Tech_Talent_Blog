75b00e59a306e6543348824376df7a03e48afa81
Storycoder <joseph.margolis@gmail.com>
2015-08-12 16:26:31 -0400

    scaffold blog post/bootstrap blog_posts/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :blog_posts
+  resources :blog_posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
