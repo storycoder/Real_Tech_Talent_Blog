81d6a9466625459fa718417644b92e89155e3372
Storycoder <joseph.margolis@gmail.com>
2015-08-13 09:19:14 -0400

    Add Value to comments updated parmas in controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812203543) do
+ActiveRecord::Schema.define(version: 20150813131509) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150812203543) do
     t.text     "comment_entry"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "blog_post_id"
   end
 
 end
